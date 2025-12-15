41d8fa66c7deb17e2931652100069ecea0122d55
Mifrahzia <mifrahtmz@gmail.com>
2025-12-16 01:57:46 +0500

    step 6

--- src/main/java/org/springframework/samples/petclinic/vet/Vet.java
+++ src/main/java/org/springframework/samples/petclinic/vet/Vet.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2025 the original author or authors.
+ * Copyright 2012-2025 the original author or authors. 
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -11,18 +11,18 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.
+ * limitations under the License. 
  */
-package org.springframework.samples.petclinic.vet;
+package org.springframework.samples.petclinic. vet;
 
-import java.util.Comparator;
+import java.util. Comparator;
 import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
+import java.util. List;
+import java.util. Set;
+import java.util. stream.Collectors;
 
-import org.springframework.samples.petclinic.model.NamedEntity;
-import org.springframework.samples.petclinic.model.Person;
+import org.springframework.samples.petclinic.model. NamedEntity;
+import org.springframework.samples.petclinic. model.Person;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.Entity;
@@ -32,7 +32,7 @@ import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import jakarta.xml.bind.annotation.XmlElement;
-import org.jspecify.annotations.Nullable;
+import org.jspecify.annotations. Nullable;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 
 /**
@@ -66,6 +66,10 @@ public class Vet extends Person {
 		return getSpecialtiesInternal().stream()
 			.sorted(Comparator.comparing(NamedEntity::getName))
 			.collect(Collectors.toList());
+	}
+
+	public void setSpecialties(Set<Specialty> specialties) {
+		this.specialties = specialties;
 	}
 
 	public int getNrOfSpecialties() {
